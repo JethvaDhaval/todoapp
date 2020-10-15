7d0ba354feb892eb42404e5a770e000219bd0eb6
JethvaDhaval <dcjethva2525@gmail.com>
2020-10-15 16:17:52 +0530

    Updates

---
+++ precache-manifest.9d70054caba792f3713fad3ef02ec805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2afab9405b456e4e9f698bbec66118",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "f3d2a475372afd5de4d3",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afabeb75e1b5d33ef251",
+    "url": "/todoapp/static/js/2.65eb93cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.65eb93cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d2a475372afd5de4d3",
+    "url": "/todoapp/static/js/main.ad9af1d2.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
